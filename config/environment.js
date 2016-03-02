28c96af9ed0718afda16eb1ef162b35e86b948a5
nerdy beast <therealnerdybeast@gmail.com>
2016-03-02 10:31:17 -0700

    Added ember simple auth for Auth0.com

--- config/environment.js
+++ config/environment.js
@@ -1,47 +1,70 @@
 /* jshint node: true */
 
 module.exports = function(environment) {
-  var ENV = {
-    modulePrefix: 'sith',
-    environment: environment,
-    baseURL: '/',
-    locationType: 'auto',
-    EmberENV: {
-      FEATURES: {
-        // Here you can enable experimental features on an ember canary build
-        // e.g. 'with-controller': true
-      }
-    },
 
-    APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
-    }
-  };
+		var ENV = {
+				modulePrefix: 'sith',
+				environment: environment,
+				baseURL: '/',
+				locationType: 'auto',
+				EmberENV: {
+						FEATURES: {
+								// Here you can enable experimental features on an ember canary build
+								// e.g. 'with-controller': true
+						}
+				},
 
-  if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
-  }
+				APP: {
+					// Here you can pass flags/options to your application instance
+					// when it is created
+				}
+		};
 
-  if (environment === 'test') {
-    // Testem prefers this...
-    ENV.baseURL = '/';
-    ENV.locationType = 'none';
+		if (environment === 'development') {
+				// ENV.APP.LOG_RESOLVER = true;
+				// ENV.APP.LOG_ACTIVE_GENERATION = true;
+				// ENV.APP.LOG_TRANSITIONS = true;
+				// ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+				// ENV.APP.LOG_VIEW_LOOKUPS = true;
+		}
 
-    // keep test console output quieter
-    ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
+		if (environment === 'test') {
+				// Testem prefers this...
+				ENV.baseURL = '/';
+				ENV.locationType = 'none';
 
-    ENV.APP.rootElement = '#ember-testing';
-  }
+				// keep test console output quieter
+				ENV.APP.LOG_ACTIVE_GENERATION = false;
+				ENV.APP.LOG_VIEW_LOOKUPS = false;
 
-  if (environment === 'production') {
+				ENV.APP.rootElement = '#ember-testing';
+		}
 
-  }
+		if (environment === 'production') {
 
-  return ENV;
+		}
+
+		//START => Auth0 options
+		//See: https://github.com/auth0/auth0-ember-simple-auth/blob/master/README.md
+		ENV['ember-simple-auth'] = {
+				authenticationRoute: 'index',
+		  	routeAfterAuthentication: 'protected',
+		  	routeIfAlreadyAuthenticated: 'protected'
+		};
+
+		ENV['auth0-ember-simple-auth'] = {
+		  	clientID: "aaCGzBtEetwZP3J0psnVh2iWDvoISdkW",
+		  	domain: "sith-oath.auth0.com"
+		};
+
+		ENV['contentSecurityPolicy'] = {
+		    'font-src': "'self' data: https://*.auth0.com",
+		    'style-src': "'self' 'unsafe-inline'",
+		    'script-src': "'self' 'unsafe-eval' https://*.auth0.com",
+		    'img-src': '*.gravatar.com *.wp.com data:',
+		    'connect-src': "'self' http://localhost:* https://sith-oath.auth0.com"
+  	};
+  	//END => Auth0 options
+
+		return ENV;
 };
